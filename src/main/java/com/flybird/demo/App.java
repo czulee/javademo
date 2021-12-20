13f7acd7a5d74b9bf471fd2c021f1f8c36a422d1
lee <li.yifei@gmail.com>
2021-12-20 14:38:53 +0800

    add README

--- src/main/java/com/flybird/demo/App.java
+++ src/main/java/com/flybird/demo/App.java
@@ -2,6 +2,6 @@ package com.flybird.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Java!!!");
     }
 }
